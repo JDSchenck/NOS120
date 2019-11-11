b21b6d5b3251a98181cb60429e8580b24d43faf2
Jason Schenck <jsche4@uis.edu>
2019-11-10 18:55:02 -0600

    Assignment10.sh

---
+++ Assignment10.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+useradd jdoe
+useradd wjones
+useradd wjenkins
+useradd tbanks
+useradd wsmith
+echo "Added new users: jdoe, wjones, wjenkins, tbanks, and wsmith"
+groupadd managers
+groupadd acctg
+echo "Add new groups: managers and acctg"
+
